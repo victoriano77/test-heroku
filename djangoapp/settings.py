8fef48e243d4b0f6db34fddc792ce0c37a7e8701
victoriano <sabueha2@gmail.com>
2021-11-15 14:37:01 -0800

    third commit

--- djangoapp/settings.py
+++ djangoapp/settings.py
@@ -122,7 +122,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR/'static']
-STATIC_ROOT = "static_root"
+
+
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
